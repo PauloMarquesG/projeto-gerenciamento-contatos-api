48cf3da8e240eca224deee502f0db23a9709374e
GuilhermeLucera <guilhermeglucera@hotmail.com>
2025-08-14 09:21:09 -0300

    initial commit

---
+++ src/main/java/com/projeto/gerenciamentocontatoapi/GerenciamentocontatoapiApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.gerenciamentocontatoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentocontatoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentocontatoapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/gerenciamentocontatoapi/GerenciamentocontatoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.gerenciamentocontatoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentocontatoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
